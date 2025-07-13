c1e1ebcc9ad883dbe8e6457db03ab4f613e647ce
jehaddayman <jehaddayman@gmail.com>
2025-07-13 21:29:24 +0300

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -8,3 +8,13 @@ def log_queries(func):
         print(f"Executing SQL query: {query}")
         return func(*args, **kwargs)
     return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
