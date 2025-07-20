28847a7faecf500ed176a3c83dcb1e4fed93f531
jehaddayman <jehaddayman@gmail.com>
2025-07-20 22:36:05 +0300

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,16 @@
+from rest_framework_nested.routers import NestedDefaultRouter
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet)
+# Router الأساسي
+router = NestedDefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+# راوتر متداخل للرسائل داخل المحادثات
+conversation_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversation_router.urls)),
 ]
