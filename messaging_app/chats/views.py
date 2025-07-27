8674d161d379d252a7344547351afa3ffc49799c
jehaddayman <jehaddayman@gmail.com>
2025-07-27 23:31:22 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,18 +1,16 @@
-from rest_framework import viewsets, permissions
-from .models import Conversation, Message
-from .serializers import ConversationSerializer, MessageSerializer
-
-
-class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
-    serializer_class = ConversationSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
+from rest_framework import viewsets
+from .models import Message
+from .serializers import MessageSerializer
+from .permissions import IsParticipantOfConversation
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsParticipantOfConversation]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Message.objects.filter(conversation__participants=user)
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
